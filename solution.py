55e2d075702e9dc5e9a5e6346384f5d938f5bd11
Zaitsev Maksim <108177495+Za1cevMaksim@users.noreply.github.com>
2023-04-08 13:46:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,26 @@
-import pandas as pd
 import numpy as np
+from scipy.stats import stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 461694118
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+
+
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    significance_level = 0.03
+    control_conversion_rate = x_success / x_cnt
+    test_conversion_rate = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+
+    SE = np.sqrt(p * (1 - p) * (1 / x_cnt + 1 / y_cnt))
+    z = (test_conversion_rate - control_conversion_rate) / SE
+
+    if z < -1 * abs(stats.norm.ppf(significance_level / 2)):
+        return False
+    elif z > abs(stats.norm.ppf(significance_level / 2)):
+        return False
+    else:
+        return True
